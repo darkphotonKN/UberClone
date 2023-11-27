b33a6951ef3115214ede6da741986e930a9724dc
Kranti <darkphoton20@gmail.com>
2023-11-27 18:50:19 +0800

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Kranti on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
